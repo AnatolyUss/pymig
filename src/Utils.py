eda9cde57cb06cd186c98db12c0074cc5ffcbf72
Anatoly <anatolyuss@gmail.com>
2019-11-09 23:33:27 +0200

    added Utils class

---
+++ src/Utils.py
@@ -0,0 +1,33 @@
+__author__ = "Anatoly Khaytovich <anatolyuss@gmail.com>"
+__copyright__ = "Copyright (C) 2018 - present, Anatoly Khaytovich <anatolyuss@gmail.com>"
+__license__ = """
+    This file is a part of "PYMIG" - the database migration tool.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program (please see the "LICENSE.md" file).
+    If not, see <http://www.gnu.org/licenses/gpl.txt>.
+"""
+
+
+class Utils:
+    @staticmethod
+    def get_index_of(element, elements):
+        """
+        Returns an index of given element in the list of elements.
+        If the element not found - returns -1.
+        :param element: variable of any type
+        :param elements: list
+        :return: int
+        """
+        try:
+            return elements.index(element)
+        except ValueError:
+            return -1
